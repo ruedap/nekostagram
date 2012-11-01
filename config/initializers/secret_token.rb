ead2b6cf2b846405c549e396782016dc2f6bcea0
ruedap <ruedap@ruedap.com>
2012-11-02 04:34:57 +0900

    Create a new project in rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nekostagram::Application.config.secret_token = 'f870b5a2ef9693b34a25755a35cfe56acc8a20257b43487593e89fd1545e5b844a9a03dd115e32a61d05f55f4ae6393040cc927c0faa0ebbc17cc31fb6abada4'
