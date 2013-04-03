e90eda91194b2744cc61c4bc6e09492d3f090a6c
ruedap <ruedap@ruedap.com>
2013-04-04 01:23:14 +0900

    Implement i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :detect_locale
+
+  private
+  def detect_locale
+    lang = request.headers['Accept-Language'].scan(/^[a-z]{2}/).first
+    I18n.locale = (lang == 'ja') ? 'ja' : 'en'
+  end
 end
