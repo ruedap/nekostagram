70934320830ab081928a0c4257040cf0db4a98c2
ruedap <ruedap@ruedap.com>
2013-05-13 21:33:59 +0900

    Fix detect_locale bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,24 @@ class ApplicationController < ActionController::Base
 
   private
   def detect_locale
-    lang = request.headers['Accept-Language'].scan(/^[a-z]{2}/).first
+    lang = accept_locale(request.env)
     I18n.locale = (lang == 'ja') ? 'ja' : 'en'
+  end
+
+  # https://github.com/rack/rack-contrib/blob/master/lib/rack/contrib/locale.rb
+  def accept_locale(env)
+    accept_langs = env["HTTP_ACCEPT_LANGUAGE"]
+    return if accept_langs.nil?
+
+    languages_and_qvalues = accept_langs.split(",").map { |l|
+      l += ';q=1.0' unless l =~ /;q=\d+(?:\.\d+)?$/
+      l.split(';q=')
+    }
+
+    lang = languages_and_qvalues.sort_by { |(locale, qvalue)|
+      qvalue.to_f
+    }.last.first
+
+    lang == '*' ? nil : lang
   end
 end
