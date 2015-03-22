a707ad487522b1b64298de13f0c6d7f9d0ec6474
ruedap <ruedap@ruedap.com>
2015-03-22 16:36:51 +0900

    Remove body_class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-
-  # Public: Generate css class for controller name and action name.
-  #
-  # Returns HTML.
-  def body_class
-    "#{controller_name}-controller #{action_name}-action".underscore.dasherize
-  end
-
   def target_tag
     ENV['INSTAGRAM_TARGET_TAG']
   end
