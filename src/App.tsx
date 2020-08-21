d2b535b0aa303bad95858e7e9051516b19a4a2fd
ruedap <ruedap@ruedap.com>
2020-08-21 22:21:14 +0900

    feat(src): Add index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.en.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Oops! It seems like cats got lost.</p>
+      <a
+        href="https://github.com/ruedap/nekostagram"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        ruedap/nekostagram
+      </a>
     </div>
   );
 }
